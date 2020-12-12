9d6d00ab314ff84131eb5b614e81832f7d42b776
Arian <arian.kulmer@web.de>
2020-12-12 09:51:11 +0100

    Covers: Refactor wallpaper generation

--- generate_smallvariants.py
+++ generate_smallvariants.py
@@ -1,43 +1,39 @@
-#!/usr/bin/env python3
-import imghdr
 import os
 
 from PIL import Image
 
-path = os.path.dirname(os.path.realpath(__file__))
+path = os.path.dirname(os.path.realpath(__file__)) + "/"
 
 resources = ["res/drawable-nodpi"]
- 
+
 def generate_smallvariants(resource):
     global path
-    wallpapers_path = os.path.join(path, resource)
+    wallpapers_path = path + resource + "/"
     clean(wallpapers_path)
     wallpapers = os.listdir(wallpapers_path)
 
     for wallpaper in wallpapers:
-        # Test if the wallpaper is a valid jpeg file
-        if imghdr.what(os.path.join(wallpapers_path, wallpaper)) != "png":
-            print(os.path.join(resource, wallpaper) + " is not a valid jpeg file")
-            continue
-
         # Append _small.jpg to the wallpaper
-        wallpaper_small = os.path.splitext(wallpaper)[0] + "_small.png"
-        wallpaper_small_path = os.path.join(wallpapers_path, wallpaper_small)
+        wallpaper_small = wallpaper[:-4] + "_small.jpg"
+        wallpaper_small_path = wallpapers_path + wallpaper_small
 
-        # Save the wallpaper with 1/4 size to wallpaper_small_path
-        with Image.open(os.path.join(wallpapers_path, wallpaper)) as img:
-            size = int(img.width / 4), int(img.height / 4)
+        # Save the wallpaper with 1/5 size to wallpaper_small_path
+        with Image.open(wallpapers_path + wallpaper) as img:
+            small_width = img.width / 5
+            small_height = img.height / 5
+
+            size = int(small_width), int(small_height)
 
             img_small = img.resize(size, Image.ANTIALIAS)
-            img_small.save(wallpaper_small_path, "PNG")
+            img_small.convert('RGB').save(wallpaper_small_path, "JPEG")
 
 def clean(wallpapers_path):
     wallpapers = os.listdir(wallpapers_path)
 
     for wallpaper in wallpapers:
         # Get rid of existing small variants
-        if wallpaper.endswith("_small.png"):
-            os.remove(os.path.join(wallpapers_path, wallpaper))
+        if wallpaper.endswith("_small.jpg"):
+            os.remove(wallpapers_path + wallpaper)
 
 for resource in resources:
     generate_smallvariants(resource)
